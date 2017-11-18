e972538c30a8f0a2dbed9b469cf72bb11e5427c1
Andrei Matusevich <>
2017-11-18 12:20:12 +0200

    Dispatcher and Scanner classes were added

---
+++ USBList/Dispatcher/USBDispatcher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using USBList.Scanner;
+using USBList.Device;
+
+namespace USBList.Dispatcher
+{
+    sealed class USBDispatcher
+    {
+        private static USBDispatcher _instance;
+        public static USBDispatcher GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new USBDispatcher();
+            }
+            return _instance;
+        }
+
+        public List<USBDevice> USBDevices { get; private set; }
+        public List<USBDevice> UpdateList()
+        {
+            USBDevices = USBScanner.GetInstance().GetUSBDevices();
+            return USBDevices;
+        }
+    }
+}
---
+++ USBList/Scanner/USBScanner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using USBList.Device;
+
+namespace USBList.Scanner
+{
+    sealed class USBScanner
+    {
+        private static USBScanner _instance;
+
+        public static USBScanner GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new USBScanner();
+            }
+            return _instance;
+        }
+
+        public List<USBDevice> GetUSBDevices()
+        {
+            //TODO
+            return null;
+        }
+
+
+    }
+}
